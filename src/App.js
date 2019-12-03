89c5af84baf31836984ef4c003fbfab2c8cfc56a
Michal Wisniewski <michal.wisniewski@softnauts.com>
2019-12-03 17:00:02 +0100

    removing and dragging tasks

--- src/App.js
+++ src/App.js
@@ -6,45 +6,59 @@ import { StateProvider } from './state/state';
 
 function App() {
   const initialState = {
-    tasks: [
-      {
-        title: 'title 1',
-        description: 'desc',
-        textColor: 'red',
-        backgroundColor: 'blue',
-        order: 0,
-        done: false,
-      },
-      {
-        title: 'title 2',
-        description: 'desc',
-        textColor: 'red',
-        backgroundColor: 'blue',
-        order: 1,
-        done: false,
-      },
-      {
-        title: 'title 3',
-        description: 'desc',
-        textColor: 'red',
-        backgroundColor: 'blue',
-        order: 2,
-        done: false,
-      },
-    ],
+    // tasks: [
+    //   {
+    //     title: 'title 1',
+    //     description: 'desc',
+    //     textColor: 'red',
+    //     backgroundColor: 'blue',
+    //     id: 0,
+    //     done: false,
+    //   },
+    //   {
+    //     title: 'title 2',
+    //     description: 'desc',
+    //     textColor: 'red',
+    //     backgroundColor: 'blue',
+    //     id: 5,
+    //     done: false,
+    //   },
+    //   {
+    //     title: 'title 3',
+    //     description: 'desc',
+    //     textColor: 'red',
+    //     backgroundColor: 'blue',
+    //     id: 7,
+    //     done: false,
+    //   },
+    // ],
+    tasks: []
   };
 
   const reducer = (state, action) => {
     switch (action.type) {
       case 'addTask':
-        console.log('yo!', action)
+        const newTaskId = state.tasks.length ? Math.max(...state.tasks.map(item => item.id)) + 1 : 0
         return {
           ...state,
           tasks: [
             ...state.tasks,
-            action.newTask
+            {
+              ...action.newTask,
+              id: newTaskId
+            }
           ]
         };
+      case 'reorderTasks':
+        return {
+          ...state,
+          tasks: action.reorderedTasks,
+        }
+      case 'removeTask':
+        return {
+          ...state,
+          tasks: state.tasks.filter(task => task.id !== action.idToRemove)
+        }
       default:
         return state;
     }
--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -4,11 +4,20 @@ import { useStateValue } from '../../state/state';
 import AddTaskModal from '../AddTaskModal/AddTaskModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import RLDD from 'react-list-drag-and-drop/lib/RLDD';
 
 
 function MainComponent() {
-  const [{ tasks }] = useStateValue();
   const [addTaskModalOpen, setAddTaskModalOpen] = useState(false);
+  const [{ tasks }, dispatch] = useStateValue();
+
+  const handleRLDDChange = (reorderedItems) => {
+    console.log(reorderedItems)
+    dispatch({
+      type: 'reorderTasks',
+      reorderedTasks: reorderedItems,
+    });
+  }
 
   return (
     <div className="main-component">
@@ -17,24 +26,31 @@ function MainComponent() {
       </header>
       <section className="tasks-list">
         <div className="containter">
-          {
-            tasks.map((task, index) => {
+
+          { !tasks.length && <div className="info-message">No tasks on the list. Use the buttom below to add some.</div> }
+          { tasks.length >= 1 && <div className="info-message">You can reorder your task just by draggineg them up or down.</div> }
+
+          <RLDD
+            items={tasks}
+            itemRenderer={task => {
               return (
-                <div className="task" key={index}>
+                <div className="task">
                   <div className="title-line">
                     <h3>{task.title}</h3>
                     <div>
                       <span><FontAwesomeIcon icon={faEdit} /></span>
-                      <span><FontAwesomeIcon icon={faTrash} /></span>
+                      <span><FontAwesomeIcon icon={faTrash} onClick={() => dispatch({type: 'removeTask', idToRemove: task.id})} /></span>
                     </div>
                   </div>
                   <div>{task.description}</div>
                 </div>
-              )
-            })
-          }
+              );
+            }}
+            onChange={handleRLDDChange}
+          />
         </div>
       </section>
+
       <section className="add-task-button">
         <button onClick={() => setAddTaskModalOpen(true)}>Add new task</button>
       </section>
